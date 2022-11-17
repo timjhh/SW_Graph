7e2eee618ce1bc2cf3aeb731d56385aff49a19c3
Tim Harrold <timjharrold@gmail.com>
2022-11-17 14:47:46 +0100

    Created Template from create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Graph from "./Graph.jsx"
+
+function App() {
+  return (
+    <Graph/>
+  );
+}
+
+export default App;
---
+++ src/Graph.jsx
@@ -0,0 +1,275 @@
+import React, { useEffect, useState } from 'react';
+import * as d3 from "d3";
+import { Table, Accordion, Card  } from 'react-bootstrap';
+
+
+
+function Graph(props) {
+
+const radius = 2;
+
+const legendLabels = ["OTC", "Prescription"]
+
+const [nodes,setNodes] = useState([]);
+const [links,setLinks] = useState({})
+
+
+
+
+// Update margin once size ref is created
+const margin = {top: 50, right: 20, bottom: 30, left: 30},
+width = 700 - margin.right - margin.left,
+height = 400 - (margin.top+margin.bottom);
+
+const MIN_RADIUS = 1;
+const MAX_RADIUS = 30;
+
+
+useEffect(() => {
+
+
+
+
+    d3.csv('./occurrences.csv')
+    .then(text  => {
+      setLinks(text)
+      console.log(text)
+      
+      fetch('./characters.txt')
+      .then((r) => r.text())
+      .then(text  => {
+        var nds = []
+        var tmp = text.split("\r\n")
+        tmp.forEach(d => nds.push({id: d}))
+        setNodes(nds)
+      }).catch(error => {
+        console.log("Error reading nodes!" + error)
+      });
+
+    }).catch(error => {
+      console.log("Error reading links")
+    });
+
+    // fetch('/characters.txt')
+    //     .then((r) => r.text())
+    //     .then(text  => {
+    //       setNodes(text.split("\r\n"))
+    //     }).catch(error => {
+    //       // Maybe present some error/failure UI to the user here
+    //     });
+
+
+
+}, []);
+
+useEffect(() => {
+  genGraph()
+}, [nodes,links])
+
+// useEffect(() => {
+
+
+//     d3.selectAll(".nodes").selectAll("circle")
+//     .attr("r", function(d) {
+//       if(props.sameScale) {
+//         let max = d3.max(props.current, d => parseFloat(d.nadac_per_unit));
+//         return ((d.nadac_per_unit / max) * MAX_RADIUS) + MIN_RADIUS;
+//       } else {
+
+//         let otcmax = d3.max(props.current, d => d.otc === "Y" && parseFloat(d.nadac_per_unit));
+//         let notcmax = d3.max(props.current, d => d.otc === "N" && parseFloat(d.nadac_per_unit));
+
+//         return MIN_RADIUS + (MAX_RADIUS*(d.otc === "Y" ? (parseFloat(d.nadac_per_unit)/otcmax) : d.nadac_per_unit/notcmax));
+
+//       }
+//     });
+
+// }, [props.sameScale]);
+
+    async function genGraph() {
+
+
+    console.log(nodes)
+    console.log(links)
+
+    d3.select("#graph").select("svg").remove();
+
+    const svg = d3.select("#graph")
+    .append("svg")
+    .attr("class", "svg-content-responsive svg-container")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .style("border", "1px solid black")
+    //.style("position", "absolute")
+    .attr("viewBox", "0 0 " + (width) + " " + (height))
+    .on("click", (event, item) => {
+        console.log(event.srcElement.tagName === "svg");
+
+        if(event.srcElement.tagName === "svg") {
+          node.attr("opacity", 1);
+        }
+
+    });
+
+    const g = svg.append("g")
+    .attr("class", "content");
+
+
+    const zoom = d3.zoom()
+        .scaleExtent([1, 8])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", (d) => {
+          g.attr("transform", d.transform)
+        });
+    
+    svg.call(zoom);
+
+  // var forceX = d3.forceX((d,idx) => {
+
+  //   let otcmax = d3.max(props.current, d => d.otc === "Y" && parseFloat(d.nadac_per_unit));
+  //   let notcmax = d3.max(props.current, d => d.otc === "N" && parseFloat(d.nadac_per_unit));
+    
+  //   return d.otc === "N" ? width-((parseFloat(d.nadac_per_unit)/notcmax)*width/2) : 
+  //     (parseFloat(d.nadac_per_unit)/otcmax)*width/2;
+
+  // }).strength(0.6);
+
+  var forceX = d3.forceX().strength(0.5)
+  var forceY = d3.forceY().strength(0.5);
+
+
+    const simulation = d3.forceSimulation()
+    .force("charge", d3.forceManyBody())
+    .force("repel", d3.forceManyBody().strength(-30))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("collision", d3.forceCollide(4))
+    .force(
+      "link", d3.forceLink().id(d => d).distance(40))
+    .force("x", forceX)
+    .force("y", forceY);
+
+    const legendX = parseFloat(margin.left/2);
+    const legendY = parseFloat(margin.top/2);
+    const legend = svg.append("g")
+    .attr("class", "legend");
+
+    legend.selectAll("path")
+    .data(legendLabels)
+    .join("circle")
+      // Manually add offset based on index of year
+      // Oh boy is this some spaghetti
+      // Note - 20 is the offset in this case, as each index is multiplied by 20
+      .attr("transform", (d,idx) => "translate(" + parseFloat(legendX-5) + "," + parseFloat((legendY-2) + (idx * 10)) + ")")
+      .attr("r", 2)
+      .attr("fill", (d,idx) => d === "OTC" ? "red" : "rgba(70,130,180,0.8)");
+
+    // Add legend text
+    legend.selectAll("text")
+    .data(legendLabels)
+    .join("text")
+      .text(d => d)
+      .attr("font-size", "0.5em")
+      .attr("font-weight", "lighter")
+      .attr("x", legendX)
+      // Manually added text offset - see above comment
+      .attr("y", (d,idx) => parseFloat((legendY) + (idx * 10)));
+
+    var node = g.append("g")
+    .attr("class", "nodes")
+    .selectAll("g")
+    .data(nodes)
+    .enter().append("g");
+
+    node.append("circle")
+    .attr("r", 10)
+    .attr("fill", d => ("rgba(70,130,180,0.8)")) // rgba is steelblue at 80% opacity
+    .on("click", (d,e) => {
+      props.setLabel(e);
+    })
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended));
+
+    var link = g.append("g")
+      .attr("class", "links")
+    .selectAll("line")
+    .data(links)
+    .join("line")
+      .attr("stroke", "rgba(211,211,211, 1)")
+      .attr("width", 1)
+      //.attr("stroke-opacity", d => (d.width/props.maxWidth)+props.minOpacity)
+      //.attr("stroke-width", function(d) { return (d.width+(0.2)); });
+
+
+    simulation
+    .nodes(nodes)
+    .on("tick", ticked);
+
+    simulation.force("link")
+    .links(links)
+
+
+
+    node.append("text")
+    .text((d) => "Hello")
+        .attr('x', 2)
+        .style("cursor", "pointer")
+        .style("font-weight", "bold")
+        .style("font-size", "0.2em")
+        .attr('y', 0);
+
+  function dragstarted(d) {
+    if (!d.active) simulation.alphaTarget(0.3).restart();
+    d.subject.fx = d.x;
+    d.subject.fy = d.y;
+  }
+
+  function dragged(d) {
+    d.subject.fx = d.x;
+    d.subject.fy = d.y;
+  }
+
+  function dragended(d) {
+    if (!d.active) simulation.alphaTarget(0);
+    d.subject.fx = null;
+    d.subject.fy = null;
+  }
+
+
+
+
+    function ticked() {
+
+        link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+        node
+        .attr("transform", function(d) {
+          return "translate(" + (d.x = Math.max(radius, Math.min(width - radius, d.x))) + "," + (d.y = Math.max(radius, Math.min(height - radius, d.y))) + ")"; 
+        });
+
+      }
+
+      // Re-compute y force on graph
+      simulation.force("y").initialize(nodes);
+
+      // Restart simulation
+      simulation
+      .alpha(0.3)
+      .alphaTarget(0)
+      .restart();
+  }
+
+  return (
+    <>
+    <div id={"graph"}>
+      
+    </div>
+    </>
+  );
+}
+
+export default Graph;
